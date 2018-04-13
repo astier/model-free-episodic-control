ee40dc5669114e834f1fc103cadf76b19cc63f36
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-04-13 21:11:22 +0200

    Change shebang to python2

--- launcher.py
+++ launcher.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 """This script handles reading command line arguments and starting the
 training process.  It shouldn't be executed directly; it is used by
 running script.
