981b67b28460661f1aeafc37ab19786530ca21cf
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-05-11 23:15:35 +0200

    Make use of color_averaging

--- mfec/experiment.py
+++ mfec/experiment.py
@@ -19,9 +19,6 @@ class Experiment(object):
         self.resize_width = resize_width
         self.resize_height = resize_height
         self.width, self.height = ale.getScreenDims()
-        self.frame_buffer = np.empty((2, self.height, self.width),
-                                     dtype=np.uint8)
-        self.frame_count = 0  # TODO get rid of this somehow!
         self.death = False  # Last episode ended because agent died
 
     def run(self):
@@ -37,10 +34,6 @@ class Experiment(object):
         if not self.death or self.ale.game_over():
             self.ale.reset_game()
 
-        # TODO fill during initialization
-        for _ in range(self.frame_buffer.shape[0]):  # fill the frame-buffer
-            self.act(0)
-
         start_lives = self.ale.lives()
         game_over = False
         reward = 0
@@ -49,7 +42,7 @@ class Experiment(object):
 
         # TODO stick with game_over and terminal
         while not game_over and steps < max_steps:
-            reward = sum(self.act(self.actions[action]) for _ in
+            reward = sum(self.ale.act(self.actions[action]) for _ in
                          range(self.frame_skip))
             self.death = (self.death_ends_episode and
                           self.ale.lives() < start_lives)
@@ -60,20 +53,8 @@ class Experiment(object):
         self.agent.end_episode(reward, game_over)
         return steps
 
-    def act(self, action):
-        """Perform an action for a single frame and store the frame."""
-        reward = self.ale.act(action)
-        index = self.frame_count % self.frame_buffer.shape[0]
-        self.ale.getScreenGrayscale(self.frame_buffer[index])
-        self.frame_count += 1
-        return reward
-
+    # TODO projection should happen here!
     def get_state(self):
-        """ Resize and merge the previous two screen images."""
-        # TODO why are two frames merged?
-        # TODO adapt to more than two frames
-        index = self.frame_count % self.frame_buffer.shape[0] - 1
-        frame = np.maximum(self.frame_buffer[index],
-                           self.frame_buffer[index - 1])
         rescale_size = (self.resize_width, self.resize_height)
+        frame = self.ale.getScreenGrayscale()[:, :, 0]
         return scipy.misc.imresize(frame, size=rescale_size)
