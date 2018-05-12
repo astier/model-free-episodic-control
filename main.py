023eec2fc4b71c08697a12e4660267fa71966cc8
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-05-12 10:51:33 +0200

    Refactor experiment.py into main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import logging
 import os
 
 import numpy as np
+import scipy.misc
 from atari_py.ale_python_interface import ALEInterface
 
 from mfec.agent import EpisodicControl
-from mfec.experiment import Experiment
 from mfec.qec import QEC
 
 # TODO parameters as json-config
@@ -32,16 +32,19 @@ DISPLAY_SCREEN = False
 QEC_TABLE = ''
 SEED = 1
 
+ale = None
+agent = None
 
-def main():  # TODO merge with experiment.py
+
+def init():
     logging.basicConfig(level=logging.INFO)
-    ale = setup_ale()
-    agent = setup_agent(len(ale.getMinimalActionSet()))
-    Experiment(ale, agent, RESIZE_WIDTH, RESIZE_HEIGHT, EPOCHS,
-               STEPS_PER_EPOCH, FRAME_SKIP).run()
+    setup_ale()
+    setup_agent(len(ale.getMinimalActionSet()))
+    train()
 
 
 def setup_ale():  # TODO ale vs gym
+    global ale
     ale = ALEInterface()
     # TODO check more variables
     ale.setInt('random_seed', SEED)
@@ -49,21 +52,57 @@ def setup_ale():  # TODO ale vs gym
     ale.setFloat('repeat_action_probability', REPEAT_ACTION_PROBABILITY)
     ale.setBool('color_averaging', True)
     ale.loadROM(os.path.join(ROMS, ROM))
-    return ale
 
 
 def setup_agent(actions):  # TODO cpickle vs json etc.
+    global agent
     rng = np.random.RandomState(seed=SEED)
     if QEC_TABLE:
         qec = cPickle.load(open(QEC_TABLE, 'r'))
     else:
         # TODO Gauss and others?
+        projection_matrix = rng.randn(PROJECTION_DIM, STATE_DIM).astype(
+            np.float32)
+        qec = QEC(KNN, BUFFER_SIZE, actions, projection_matrix)
+    agent = EpisodicControl(qec, DISCOUNT, actions, EPSILON, EPSILON_MIN,
+                            EPSILON_DECAY, ROM, rng)
 
-        projection = rng.randn(PROJECTION_DIM, STATE_DIM).astype(np.float32)
-        qec = QEC(KNN, BUFFER_SIZE, actions, projection)
-    return EpisodicControl(qec, DISCOUNT, actions, EPSILON, EPSILON_MIN,
-                           EPSILON_DECAY, ROM, rng)
+
+def train():
+    for epoch in range(1, EPOCHS + 1):
+        # TODO steps vs frames
+        steps_left = STEPS_PER_EPOCH
+        while steps_left > 0:
+            logging.info("Epoch: {}\tSteps: {}".format(epoch, steps_left))
+            steps_left -= run_episode(steps_left)
+        agent.end_epoch(epoch)
+
+
+def run_episode(max_steps):
+    total_reward = 0
+    steps = 0
+    agent.start_episode()
+
+    while not ale.game_over() and steps < max_steps:
+        observation = ale.getScreenGrayscale()[:, :, 0]
+        state = projection(observation)
+        action = agent.act(state)
+        reward = sum([ale.act(action) for _ in range(FRAME_SKIP)])
+        agent.add_experience(state, action, reward)
+        total_reward += reward
+        steps += 1
+
+    ale.reset_game()
+    agent.end_episode(total_reward)
+    return steps
+
+
+# TODO projection should happen here!
+def projection(observation):
+    size = (RESIZE_WIDTH, RESIZE_HEIGHT)
+    # TODO other method than scipy?
+    return scipy.misc.imresize(observation, size=size)
 
 
 if __name__ == "__main__":
-    main()
+    init()
