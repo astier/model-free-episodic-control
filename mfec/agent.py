6488305b7ade66f579e30813c6ca2fb80582d4c9
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-05-13 13:42:26 +0200

    Randomize action-selection for multiple max-value-actions

--- mfec/agent.py
+++ mfec/agent.py
@@ -29,23 +29,26 @@ class MFECAgent(object):
         self.current_state = np.dot(self.projection, observation.flatten())
         # TODO generator?
         self.epsilon = max(self.epsilon_min, self.epsilon - self.epsilon_rate)
-        if np.random.rand() > self.epsilon:
+        if np.random.rand() > self.epsilon:  # TODO expose bug by <
             self.current_action = self._exploit()
         else:
             self.current_action = np.random.choice(self.actions)
         return self.current_action
 
     def _exploit(self):
-        """Determine the action with the highest q-value."""
-        best_value = float('-inf')
-        best_action = 0
+        """Determine the action with the highest Q-value. If multiple
+        actions with the the highest value exist then choose from this set
+        of actions randomly."""
+        best_values = [(None, float('-inf'))]
         for action in self.actions:
             value = self.qec.estimate(self.current_state, action)
-            # TODO random choice for multiple actions with same value
+            best_value = best_values[0][1]
             if value > best_value:
-                best_value = value
-                best_action = action
-        return best_action
+                best_values = [(action, value)]
+            if value == best_value:
+                best_values.append((action, value))
+        action = best_values[np.random.choice(range(len(best_values)))][0]
+        return action
 
     def receive_reward(self, reward):
         """Store (state, action, reward) tuple in memory."""
