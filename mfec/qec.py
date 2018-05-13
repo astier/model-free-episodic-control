6fc43206df165e77a6e5d1c7235a18d28acfe623
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-05-13 19:40:18 +0200

    Simplify qec.py

--- mfec/qec.py
+++ mfec/qec.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2
 
+import time
 import numpy as np
-from sklearn.neighbors.kd_tree import KDTree
+from sklearn.neighbors.kd_tree import KDTree  # TODO pyflann??
 
 
 class QEC(object):
@@ -13,63 +14,64 @@ class QEC(object):
 
     def estimate(self, state, action):
         a_buffer = self.buffers[action]
-        q_value = a_buffer.get_state_value(state)
-        if q_value is not None:
-            return q_value
+        value = a_buffer.stored_value(state)[1]
+        if value:
+            return value
         if a_buffer.curr_capacity < self.knn:  # TODO delegate to agent
             return float('inf')
-        return a_buffer.knn_value(state, self.knn)
+        return a_buffer.estimated_value(state, self.knn)
 
-    def update(self, state, action, value_new):
-        value_old = self.buffers[action].get_state_value(state)
-        self.buffers[action].add(state, max(value_old, value_new))
+    # TODO batch update
+    def update(self, state, action, new_value):
+        a_buffer = self.buffers[action]
+        state_index, old_value = a_buffer.stored_value(state)
+        if state_index:
+            a_buffer.insert(state, max(old_value, new_value), state_index)
+        else:
+            a_buffer.add(state, new_value)
 
 
 class ActionBuffer(object):
 
     def __init__(self, capacity, state_dimension):
-        self.capacity = capacity
+        self.tree = None
         self.curr_capacity = 0
-
+        self.capacity = capacity
         # TODO set of dictionaries
         self.states = np.zeros((capacity, state_dimension))
         self.q_values = np.zeros(capacity)
         self.lru = np.zeros(capacity)
 
-        self.tree = None  # TODO init here!
-        self.tm = .0
-
-    def get_state_value(self, state):
+    def stored_value(self, state):
         if self.curr_capacity > 0:
-            # tree = KDTree(self.states[:self.curr_capacity])
-            _, closest_neighbors_indices = self.tree.query([state], k=1)
-            closest_neighbor_index = closest_neighbors_indices[0][0]
-            if np.allclose(self.states[closest_neighbor_index], state):
-                self.lru[closest_neighbor_index] = self.tm
-                self.tm += .01
-                return self.q_values[closest_neighbor_index]
-        return None
+            state_index = self.tree.query([state])[1][0][0]
+            # TODO check np.allclose
+            if np.allclose(self.states[state_index], state):
+                self.lru[state_index] = time.clock()
+                return state_index, self.q_values[state_index]
+        return None, None  # TODO None, None is ugly
 
-    def knn_value(self, key, knn):
-        # tree = KDTree(self.states[:self.curr_capacity])
-        _, ind = self.tree.query([key], k=knn)
+    def estimated_value(self, state, knn):
+        closest_neighbors_indices = self.tree.query([state], k=knn)[1][0]
         value = .0
-        for index in ind[0]:
-            value += self.q_values[index]
-            self.lru[index] = self.tm
-            self.tm += .01
+        for state_index in closest_neighbors_indices:
+            value += self.q_values[state_index]
+            self.lru[state_index] = time.clock()
         return value / knn
 
-    def add(self, key, value):
-        if self.curr_capacity >= self.capacity:
-            old_index = np.argmin(self.lru)
-            self.states[old_index] = key
-            self.q_values[old_index] = value
-            self.lru[old_index] = self.tm
-        else:
-            self.states[self.curr_capacity] = key
-            self.q_values[self.curr_capacity] = value
-            self.lru[self.curr_capacity] = self.tm
+    def add(self, state, value):
+        if self.curr_capacity < self.capacity:
+            self.insert(state, value, self.curr_capacity)
             self.curr_capacity += 1
-        self.tm += 0.01
+        else:
+            self.insert(state, value, np.argmin(self.lru))
+
+        # TODO smarter tree-update
+        # memory ~ n_samples / leaf_size, default_leaf_size=40
         self.tree = KDTree(self.states[:self.curr_capacity])
+
+    # TODO mean between states?
+    def insert(self, state, value, index):
+        self.states[index] = state
+        self.q_values[index] = value
+        self.lru[index] = time.clock()  # TODO delete here?
