3de19f4ea8989b51459d0c89806bdd3609648200
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-06-23 10:57:38 +0200

    Round avg-reward

--- mfec/utils.py
+++ mfec/utils.py
@@ -5,9 +5,7 @@ import pickle
 import time
 
 
-# TODO improve output and stats? library?
-# TODO store detailed-output
-# TODO round reward-avg
+# TODO library?
 class Utils:
 
     def __init__(self, rom_file_name, frames_per_epoch, max_frames,
@@ -48,6 +46,7 @@ class Utils:
         self.epoch_episodes += 1
         self.epoch_frames += episode_frames
         self.epoch_reward_sum += episode_reward
+
         if episode_reward > self.epoch_reward_max:
             self.epoch_reward_max = episode_reward
         self.total_frames += episode_frames
@@ -55,20 +54,23 @@ class Utils:
         results = [self.epoch, self.epoch_episodes, episode_reward,
                    self.epoch_frames, self.frames_per_epoch]
         message = 'Epoch: {}\tEpisode: {}\tReward: {}\tEpoch-Frames: {}/{}'
+
         print(message.format(*results))
 
     def end_epoch(self):
         """Save the results for the given epoch in the results-file"""
         results = [self.epoch, self.epoch_episodes, self.epoch_frames,
                    self.epoch_reward_sum,
-                   self.epoch_reward_sum / self.epoch_episodes,
+                   round(self.epoch_reward_sum / self.epoch_episodes),
                    self.epoch_reward_max]
+
         self.results_file.write('{},{},{},{},{},{}\n'.format(*results))
         self.results_file.flush()
 
         message = '\nEpoch: {}\tEpisodes: {}\tFrames: {}\tReward-Sum: {}\t' \
                   'Reward-Avg: {}\tReward-Max: {}\tTotal-Frames: {}/{}\n'
         results = results + [self.total_frames, self.max_frames]
+
         print(message.format(*results))
 
         if self.store_agent:
@@ -80,7 +82,6 @@ class Utils:
         self.epoch_reward_sum = 0
         self.epoch_reward_max = 0
 
-    # TODO keep the best and the newest agent
     def save_agent(self):
         """Save the agents QEC-table in a file."""
         qec_prefix = os.path.join(self.results_dir, 'qec_')
