4455a6d74c5c138ffe44ea068dff9ff78796590d
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-11-14 17:36:00 +0100

    Create main-method

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from mfec.utils import Utils
 
 ENVIRONMENT = 'Qbert-v0'  # More games at: https://gym.openai.com/envs/#atari
 AGENT_PATH = 'agents/Qbert-v0_1542210528/agent.pkl'
-RENDER = True
+RENDER = False
 RENDER_SPEED = .04
 
 EPOCHS = 11
@@ -30,25 +30,46 @@ SCALE_HEIGHT = 84
 SCALE_WIDTH = 84
 STATE_DIMENSION = 64
 
-env = None
-agent = None
-utils = None
-agent_dir = None
+
+def main():
+    random.seed(SEED)
+
+    # Create agent-directory
+    execution_time = str(round(time.time()))
+    agent_dir = os.path.join('agents', ENVIRONMENT + '_' + execution_time)
+    os.makedirs(agent_dir)
+
+    # Initialize utils, environment and agent
+    utils = Utils(agent_dir, FRAMES_PER_EPOCH, EPOCHS * FRAMES_PER_EPOCH)
+    env = gym.make(ENVIRONMENT)
+
+    try:
+        env.env.frameskip = FRAMESKIP
+        env.env.ale.setFloat('repeat_action_probability', REPEAT_ACTION_PROB)
+        agent = MFECAgent.load(AGENT_PATH) if AGENT_PATH else MFECAgent(
+            ACTION_BUFFER_SIZE, K, DISCOUNT, EPSILON,
+            SCALE_HEIGHT, SCALE_WIDTH, STATE_DIMENSION,
+            range(env.action_space.n), SEED)
+        run_algorithm(agent, agent_dir, env, utils)
+
+    finally:
+        utils.close()
+        env.close()
 
 
-def run_algorithm():
+def run_algorithm(agent, agent_dir, env, utils):
     frames_left = 0
     for _ in range(EPOCHS):
         frames_left += FRAMES_PER_EPOCH
         while frames_left > 0:
-            episode_frames, episode_reward = run_episode()
+            episode_frames, episode_reward = run_episode(agent, env)
             frames_left -= episode_frames
             utils.end_episode(episode_frames, episode_reward)
         utils.end_epoch()
         agent.save(agent_dir)
 
 
-def run_episode():
+def run_episode(agent, env):
     episode_frames = 0
     episode_reward = 0
 
@@ -74,23 +95,4 @@ def run_episode():
 
 
 if __name__ == '__main__':
-    random.seed(SEED)
-
-    execution_time = str(round(time.time()))
-    agent_dir = os.path.join('agents', ENVIRONMENT + '_' + execution_time)
-    os.makedirs(agent_dir)
-
-    utils = Utils(agent_dir, FRAMES_PER_EPOCH, EPOCHS * FRAMES_PER_EPOCH)
-    env = gym.make(ENVIRONMENT)
-
-    try:
-        env.env.frameskip = FRAMESKIP
-        env.env.ale.setFloat('repeat_action_probability', REPEAT_ACTION_PROB)
-        agent = MFECAgent.load(AGENT_PATH) if AGENT_PATH else MFECAgent(
-            ACTION_BUFFER_SIZE, K, DISCOUNT, EPSILON,
-            SCALE_HEIGHT, SCALE_WIDTH, STATE_DIMENSION,
-            range(env.action_space.n), SEED)
-        run_algorithm()
-    finally:
-        utils.close()
-        env.close()
+    main()
